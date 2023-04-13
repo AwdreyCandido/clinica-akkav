29badf6bdc09067f418f3d07af1450f9c8e261f8
Karine Vitória <karinevitoria0073@gmail.com>
2023-04-13 17:12:31 -0300

    updating clinics routes

--- services/clinics.ts
+++ services/clinics.ts
@@ -1,48 +1,38 @@
 import axios from "axios";
 
-export async function getAllClinics() {
-  const res = await fetch("/api/clinics", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export async function getClinic(id: any) {
+  const res = await axios.post("/api/clinics/get-clinic", id);
+  console.log("🚀 ~ file: clinics.ts:5 ~ getClinic ~ res:", res);
 
-  const response = await res.json();
-  console.log("🚀 ~ file: clinics.ts:10 ~ getAllClinics ~ response:", response);
-  return response;
+  return res.data;
+}
+
+export async function getAllClinics() {
+  const res = await axios.get("/api/clinics/get-all-clinics");
+  console.log("🚀 ~ file: clinics.ts:5 ~ getAllClinics ~ res:", res);
+
+  return res.data;
 }
 
 export async function createNewClinic(newClinic: any) {
-  const res = await fetch("/api/clinics", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newClinic),
-  });
+  const res = await axios.post("/api/clinics/create-clinic", newClinic);
+  console.log("🚀 ~ file: clinics.ts:12 ~ createNewClinic ~ res:", res);
 
-  const response = await res.json();
-  console.log(
-    "🚀 ~ file: clinics.ts:10 ~ createNewClinic ~ response:",
-    response
-  );
+  return res.data;
 }
 
 export async function updateClinic(newClinic: any) {
-  const res = await fetch("/api/clinics", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newClinic),
-  });
+  const res = await axios.put("/api/clinics/update-clinic", newClinic);
+  console.log("🚀 ~ file: clinics.ts:19 ~ updateClinic ~ res:", res);
 
-  const response = await res.json();
-  console.log("🚀 ~ file: clinics.ts:43 ~ updateClinic ~ response:", response);
+  return res.data;
 }
 
-export async function deleteClinic(newClinic: any) {
-  const res = axios.delete("/api/clinics", newClinic);
+export async function deleteClinic(id: any) {
+  const res = await axios.delete("/api/clinics/delete-clinic", {
+    params: { id: id },
+  });
   console.log("🚀 ~ file: clinics.ts:48 ~ deleteClinic ~ res:", res);
+
+  return res.data;
 }
