3e440598f98bd215cdb159727ce3198fd8fcf1ea
Vinicius Viana de Araujo Silva <viniciusviana19@hotmail.com>
2023-04-14 01:18:05 -0300

    Editing CreateClinic form component

--- components/forms/CreateClinic.tsx
+++ components/forms/CreateClinic.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Backdrop from "../utils/Backdrop";
 import { ClinicData } from "@/declaration";
 import { createNewClinic } from "@/services/clinics";
 import { createCode } from "@/services/utils";
+import { HiArrowLeft } from "react-icons/hi2";
 
 // CodCli: 9999998,
 // NomeCli: "Clínica Sede1",
@@ -10,7 +10,9 @@ import { createCode } from "@/services/utils";
 // Telefone: "(81) 2658-7561",
 // Email: "clinicased2@email.com",
 
-const CreateClinic = () => {
+const CreateClinic: React.FC<{
+  closeModal: () => void;
+}> = (props) => {
   const [formData, setFormData] = useState({});
 
   function getValue(event: React.ChangeEvent<HTMLInputElement>) {
@@ -29,67 +31,77 @@ const CreateClinic = () => {
     event.preventDefault();
 
     const clinic = Object.assign({ CodCli: createCode() }, { ...formData });
-    console.log("🚀 ~ file: CreateClinic.tsx:32 ~ onSubmit ~ clinic:", clinic)
+    console.log("🚀 ~ file: CreateClinic.tsx:32 ~ onSubmit ~ clinic:", clinic);
 
     createNewClinic(clinic);
+    props.closeModal()
   }
 
   return (
     // <Backdrop>
-    <div className="flex bg-blue-primary justify-center items-center text-black-80 w-full bg-transparent">
-      <section
-        className={`absolute z-[130] top-0 mt-[12rem] overflow-auto duration-300 w-fit`}
-      >
-        <form className="flex flex-col gap-4 p-8 rounded-2xl max-w-[40rem] bg-white border border-blue-light">
-          <h1 className="text-ph font-medium">Cadastrar Clínica</h1>
+
+        <form className="flex flex-col gap-4 p-8 rounded-3xl max-w-[40rem] bg-white border border-blue-light">
+          <h1 className="text-ph flex items-center gap-8 font-medium">
+            <HiArrowLeft
+              className="cursor-pointer"
+              onClick={props.closeModal}
+            />
+            Cadastrar Clínica
+          </h1>
+
           <div>
             <h3 className="text-qh mb-2">Nome</h3>
             <input
               name="NomeCli"
+              placeholder="Ex: Clínica AKKAV"
               onChange={getValue}
-              className="bg-body w-full text-qh h-[3.6rem] px-4 rounded-xl"
+              className="bg-body w-full text-qh h-[3.3rem] placeholder:text-base text-base px-4 rounded-lg"
             />
           </div>
           <div>
-            <h3 className="text-qh mb-2">Endereco</h3>
+            <h3 className="text-qh mb-2">Endereço</h3>
             <input
               name="Endereco"
+              placeholder="Ex: Av. Gatinho fofinho, nº2"
               onChange={getValue}
-              className="bg-body w-full text-qh h-[3.6rem] px-4 rounded-xl"
+              className="bg-body w-full text-qh h-[3.3rem] placeholder:text-base text-base px-4 rounded-lg"
             />
           </div>
           <div>
             <h3 className="text-qh mb-2">Telefone</h3>
             <input
               name="Telefone"
+              placeholder="Ex: (99) 99999-9999"
               onChange={getValue}
               type="tel"
               maxLength={15}
-              className="bg-body w-full text-qh h-[3.6rem] px-4 rounded-xl"
+              className="bg-body w-full text-qh h-[3.3rem] placeholder:text-base text-base px-4 rounded-lg"
             />
           </div>
           <div>
-            <h3 className="text-qh mb-2">Email</h3>
+            <h3 className="text-qh mb-2">E-mail</h3>
             <input
               name="Email"
+              placeholder="Ex: clinica@akkav.com.br"
               onChange={getValue}
-              className="bg-body w-full text-qh h-[3.6rem] px-4 rounded-xl"
+              className="bg-body w-full text-qh h-[3.3rem] placeholder:text-base text-base px-4 rounded-lg"
             />
           </div>
           <div className="mt-8 flex gap-8 justify-end">
-            <button className="text-base bg-red-primary text-white py-4 px-8 rounded-xl">
+            <button
+              onClick={props.closeModal}
+              className="text-base bg-red-primary text-white py-6 px-8 rounded-3xl"
+            >
               Cancelar
             </button>
             <button
               onClick={onSubmit}
-              className="text-base bg-blue-primary text-white py-4 px-8 rounded-xl"
+              className="text-base w-full bg-blue-primary text-white py-6 px-8 rounded-3xl"
             >
               Cadastrar
             </button>
           </div>
         </form>
-      </section>
-    </div>
     // </Backdrop>
   );
 };
