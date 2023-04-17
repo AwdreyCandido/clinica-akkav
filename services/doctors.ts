7c64039887b2a17aa5e83846562109d632a3de42
Kevin <devkevferreira@gmail.com>
2023-04-17 16:22:20 -0300

    Implementing new doctor control service

--- services/doctors.ts
+++ services/doctors.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosPromise } from "axios";
 
 export async function getDoctor(id: any) {
   try {
     const res = await axios.post("/api/doctors/get-doctor", id);
-    console.log("🚀 ~ file: doctors.ts:7 ~ getDoctor ~ res:", res);
     return res.data;
   } catch (error: any) {
     console.log("🚀 ~ file: doctors.ts:9 ~ getDoctor ~ error:", error);
@@ -12,13 +11,43 @@ export async function getDoctor(id: any) {
 }
 
 export async function getAllDoctors() {
-  const res = await axios.get("/api/doctors/get-all-doctors");
-  console.log("🚀 ~ file: doctors.ts:16 ~ getAllDoctors ~ res:", res);
-  return res.data;
+  try {
+    const res = await axios.get("/api/doctors/get-all-doctors");
+    return res.data;
+  } catch (error: any) {
+    console.log("🚀 ~ file: doctors.ts:20 ~ getAllDoctors ~ error:", error);
+    return error.response;
+  }
 }
 
 export async function createNewDoctor(newDoctor: any) {
-  const res = await axios.post("/api/doctors/create-doctor", newDoctor);
-  console.log("🚀 ~ file: doctors.ts:29 ~ createNewDoctor ~ res:", res);
-  return res;
+  try {
+    const res = await axios.post("/api/doctors/create-doctor", newDoctor);
+    return res;
+  } catch (error: any) {
+    console.log("🚀 ~ file: doctors.ts:30 ~ createNewDoctor ~ error:", error);
+    return error.response;
+  }
+}
+
+export async function updateDoctor(newDoctor: any) {
+  try {
+    const res = await axios.put("/api/doctors/update-doctor", newDoctor);
+    return res.data;
+  } catch (error: any) {
+    console.log("🚀 ~ file: doctors.ts:41 ~ updateDoctor ~ error:", error);
+    return error.response;
+  }
+}
+
+export async function deleteDoctor(CodMed: any) {
+  try {
+    const res = await axios.delete("/api/doctors/delete-doctor", {
+      params: { CodMed },
+    });
+    return res.data;
+  } catch (error: any) {
+    console.log("🚀 ~ file: doctors.ts:54 ~ deleteDoctor ~ error:", error);
+    return error.response;
+  }
 }
