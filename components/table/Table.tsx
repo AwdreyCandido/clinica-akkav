63b7763c87bdd54ef80eff89d809691e0c8334c8
Kevin <devkevferreira@gmail.com>
2023-04-17 16:18:09 -0300

    Editing Table component

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -4,6 +4,7 @@ import IconButton from "../buttons/icon-button/IconButton";
 import { TbTrash } from "react-icons/tb";
 import CentralModal from "../modals/CentralModal";
 import { deleteDoctorFromClinic } from "@/services/clinics";
+import DeleteAlert from "../modals/DeleteAlert";
 
 const Table: React.FC<{ list: any; type: string }> = ({ list, type }) => {
   const [showAlert, setShowAlert] = useState(false);
@@ -24,7 +25,7 @@ const Table: React.FC<{ list: any; type: string }> = ({ list, type }) => {
   }
 
   return (
-    <section className="flex flex-col gap-4 border border-black-20 rounded-3xl overflow-hidden">
+    <section className="flex flex-col gap-4 border border-black-20 rounded-3xl overflow-hidden bg-[#f0f0f0]">
       <table>
         <tbody>
           {type === "clinic" ? (
@@ -72,7 +73,10 @@ const Table: React.FC<{ list: any; type: string }> = ({ list, type }) => {
             ? list?.map((item: DoctorData) => {
                 console.log(item);
                 return (
-                  <tr className="gap-8 bg-white p-4 [&:not(:last-child)]:border-b border-b-blue-light rounded-lg text-qh">
+                  <tr
+                    key={item.CodCli}
+                    className="gap-8 bg-white p-4 [&:not(:last-child)]:border-b border-b-blue-light rounded-lg text-qh"
+                  >
                     <td className="pl-12 py-4">{item.NomeMed}</td>
                     <td className="py-4">{item.Email}</td>
                     <td className="py-4">{item.Telefone}</td>
@@ -91,7 +95,10 @@ const Table: React.FC<{ list: any; type: string }> = ({ list, type }) => {
             : list?.map((item: PacientData) => {
                 console.log(item);
                 return (
-                  <tr className="gap-8 bg-white p-4 [&:not(:last-child)]:border-b border-b-blue-light rounded-lg text-qh">
+                  <tr
+                    key={item.CpfPaciente}
+                    className="gap-8 bg-white p-4 [&:not(:last-child)]:border-b border-b-blue-light rounded-lg text-qh"
+                  >
                     <td className="pl-12 py-4">{item.NomePac}</td>
                     <td className=" pr-12 py-4">{item.CpfPaciente}</td>
                     <td className="py-4">{item.Email}</td>
@@ -122,30 +129,3 @@ const Table: React.FC<{ list: any; type: string }> = ({ list, type }) => {
 };
 
 export default Table;
-
-const DeleteAlert: React.FC<{
-  onClose: () => void;
-  onConfirm: () => void;
-  isOpen?: boolean;
-}> = ({ onClose, onConfirm }) => {
-  return (
-    <div className="p-8 text-qh text-center rounded-3xl bg-white border border-blue-light">
-      <h2>Tem certeza que deseja excluir esse médico?</h2>
-      <p className="text-base">Essa ação não poderá ser desfeita...</p>
-      <div className="mt-8 flex gap-8 justify-end">
-        <button
-          onClick={onClose}
-          className="text-base bg-red-primary text-white py-5 px-8 rounded-3xl"
-        >
-          Cancelar
-        </button>
-        <button
-          onClick={onConfirm}
-          className=" flex-grow-[2] text-base w-full bg-blue-primary text-white py-5 px-8 rounded-3xl"
-        >
-          Confirmar
-        </button>
-      </div>
-    </div>
-  );
-};
