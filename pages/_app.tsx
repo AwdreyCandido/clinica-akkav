2f3d105554d06508a9147a393213327b62b61d71
Kevin <devkevferreira@gmail.com>
2023-04-17 16:29:25 -0300

    Implementing redirect function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname === "/" || router.pathname.includes("/_error")) {
+      router.push("/clinics");
+    }
+  }, []);
+  
   return (
     <>
       <Head>
-        <title>Clinica AKKAV</title>
+        <title>Clínica AKKAV</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
